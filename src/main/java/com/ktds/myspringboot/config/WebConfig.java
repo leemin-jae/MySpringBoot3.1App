1a1e33a29d453d8c31df0508b2945de51ec333c2
이민재 <uiui96@naver.com>
2023-08-18 15:43:28 +0900

    spring cos setting

--- src/main/java/com/ktds/myspringboot/config/WebConfig.java
+++ src/main/java/com/ktds/myspringboot/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ktds.myspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,4 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/mobile/")
                 .setCachePeriod(20);//20초
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:18080")
+                .allowedMethods("*");;
+    }
+
+
 }
